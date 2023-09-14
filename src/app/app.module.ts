526f00d4702d5927d0240115ca02efdd6463630d
evengyl <you@example.com>
2023-09-14 16:04:49 +0200

    demo signal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoSignalsComponent } from './demo-signals/demo-signals.component';
+import {HttpClientModule} from '@angular/common/http';
+import { DemoObsSignalsComponent } from './demo-obs-signals/demo-obs-signals.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoSignalsComponent,
+    DemoObsSignalsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
